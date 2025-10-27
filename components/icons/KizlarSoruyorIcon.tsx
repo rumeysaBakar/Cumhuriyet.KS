5b09b87b25e6947d20a08c148c27b194c2cad7d5
Rumeysa bakar <rumeysa.bakar.81@hotmail.com>
2025-10-27 18:20:21 +0300

    Create KizlarSoruyorIcon.tsx

---
+++ components/icons/KizlarSoruyorIcon.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const KizlarSoruyorIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
+    <path d="M12 6c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zm0 4c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z"/>
+    <path d="M12 13c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4zm6 5H6v-.57c.33-1.12 3.2-2.43 6-2.43s5.67 1.31 6 2.43V18z"/>
+  </svg>
+);
+
+export default KizlarSoruyorIcon;
